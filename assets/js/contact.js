bc37ee8a6a37a1d76d20b16c6d2937b3d615b373
Yashmita <55138349+yashmita@users.noreply.github.com>
2021-09-19 15:38:02 +0530

    Update contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -35,14 +35,3 @@ function getInputVal(id) {
     return document.getElementById(id).value;
 }
 
-function logOut(){
-	if(github === 1 || googlelogin == 1){
-	  firebase.auth().signOut().then(()=>{
-		alert('User logged out successfully')
-	  }).catch(e=>{
-		console.log(e.message);
-	  })
-	}
-	localStorage.removeItem("userLogin");
-	window.location.replace("../index.html"); 
-  }
